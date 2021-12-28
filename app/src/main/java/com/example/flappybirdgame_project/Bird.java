2be5c70dd315afb08752464bbf16ae0a83f3167f
manahil <bitf18a507@pucit.edu.pk>
2021-12-28 20:39:51 +0500

    12th commit

--- app/src/main/java/com/example/flappybirdgame_project/Bird.java
+++ app/src/main/java/com/example/flappybirdgame_project/Bird.java
@@ -2,19 +2,32 @@ package com.example.flappybirdgame_project;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 
 import java.util.ArrayList;
 
 public class Bird extends BaseObject{
+    private int count,vflap,idCurrentBitmap;
+    private float drop;
 private ArrayList<Bitmap> arrBms=new ArrayList<>();
     public Bird()
     {
+        this.count=0;
+        this.vflap=5;
+        this.idCurrentBitmap=0;
+        this.drop=0;
 
     }
     public void draw(Canvas canvas)
     {
+        drop();
         canvas.drawBitmap(this.getBm(),this.x,this.y,null);
 
+    }
+
+    private void drop() {
+        this.drop+=0.6;
+        this.y+=this.drop;
     }
 
     public ArrayList<Bitmap> getArrBms() {
@@ -31,6 +44,44 @@ private ArrayList<Bitmap> arrBms=new ArrayList<>();
 
     @Override
     public Bitmap getBm() {
-        return this.getArrBms().get(0);
+        count++;
+        if(this.count==this.vflap)
+        {
+            for(int i=0;i< arrBms.size();i++)
+            {
+                if(i==arrBms.size()-1)
+                {
+                    this.idCurrentBitmap=0;
+                    break;
+                }
+                else if(this.idCurrentBitmap==i)
+                {
+                    idCurrentBitmap=i+1;
+                    break;
+                }
+            }
+            count=0;
+        }
+        if(this.drop<0)
+        {
+            Matrix matrix=new Matrix();
+            matrix.postRotate(-25);
+            return Bitmap.createBitmap(arrBms.get(idCurrentBitmap),0,0,arrBms.get(idCurrentBitmap).getWidth(),arrBms.get(idCurrentBitmap).getHeight());
+        }
+        else if(drop>=0)
+        {
+            Matrix matrix=new Matrix();
+            if(drop<70)
+            {
+                matrix.postRotate(-25+(drop*2));
+            }else{
+                matrix.postRotate(45);
+            }
+            return Bitmap.createBitmap(arrBms.get(idCurrentBitmap),0,0,arrBms.get(idCurrentBitmap).getWidth(),arrBms.get(idCurrentBitmap).getHeight());
+
+        }
+       return this.arrBms.get(idCurrentBitmap);
+
+
     }
 }
