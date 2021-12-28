e2c1c089ff4c54db6f795695f6e30ae45e39f30b
manahil <bitf18a507@pucit.edu.pk>
2021-12-28 19:32:24 +0500

    7th commit

--- app/src/main/java/com/example/flappybirdgame_project/Bird.java
+++ app/src/main/java/com/example/flappybirdgame_project/Bird.java
@@ -1,4 +1,18 @@
 package com.example.flappybirdgame_project;
 
-public class Bird {
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.ArrayList;
+
+public class Bird extends BaseObject{
+private ArrayList<Bitmap> arrBms=new ArrayList<>();
+    public Bird()
+    {
+
+    }
+    public void draw(Canvas canvas)
+    {
+
+    }
 }
