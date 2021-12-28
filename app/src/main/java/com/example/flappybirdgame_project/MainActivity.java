a7a49326379fc4f81b347110ffb0c869c21070c2
manahil <bitf18a507@pucit.edu.pk>
2021-12-28 19:18:39 +0500

    4th commit

--- app/src/main/java/com/example/flappybirdgame_project/MainActivity.java
+++ app/src/main/java/com/example/flappybirdgame_project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.flappybirdgame_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        DisplayMetrics dm=new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_WIDTH=dm.widthPixels;
+        Constants.SCREEN_LENGTH=dm.heightPixels;
+        
+
+
+
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
