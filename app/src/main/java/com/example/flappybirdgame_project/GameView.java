882355bef6fb1dc4e9a21d8476206209e64f236f
manahil <bitf18a507@pucit.edu.pk>
2021-12-28 18:37:39 +0500

    #rd commit

---
+++ app/src/main/java/com/example/flappybirdgame_project/GameView.java
@@ -0,0 +1,19 @@
+package com.example.flappybirdgame_project;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public void draw(Canvas canvas)
+    {
+        super.draw(canvas);
+
+    }
+}
